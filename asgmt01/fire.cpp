8bce2628277e8d2a45e87b9ad644d6eafb846695
JacobTiderman <jacoberiktiderman@gmail.com>
2017-10-12 18:44:54 -0700

    Renamed test.cpp to coordinateTest.cpp Created fireTest.cpp -- not yet functional Changed fire.cpp -- Need to finish list.cpp inorder to finish fire.cpp

--- asgmt01/fire.cpp
+++ asgmt01/fire.cpp
@@ -7,15 +7,29 @@ using namespace std;
 const int		Fire::columnWidths[4] = { -1, -1, -1, -1 };
 
 Fire::Fire(const District district, const char * const name,
-	int latDeg, int latMin, int latSec, int longDeg, int longMin, int longSec)
+	int latDeg, int latMin, int latSec, int longDeg, int longMin, int longSec) :
+	district{district},
+	latitude{Coordinate(latDeg, latMin, latSec)},
+	longitude{Coordinate(longDeg, longMin, longSec)}
 {
 	// your code here, or in this constructor's initialization list
+	char* test = strdup(name);
+	strcpy(test, this->name);
+	delete test;
+	//latitude = Coordinate(latDeg, latMin, latSec);
+	//longitude = Coordinate(longDeg, longMin, longSec);
 }
 
 Fire::Fire(const District district, const char * const name,
-		   Coordinate latitude, Coordinate longitude)
+		   Coordinate latitude, Coordinate longitude) :
+	district{district},
+	latitude{latitude},
+	longitude{longitude}
 {
 	// your code here, or in this constructor's initialization list
+	char* test = strdup(name);
+	strcpy(test, this->name);
+	delete test;
 }
 
 Fire::~Fire()
@@ -26,6 +40,8 @@ Fire::~Fire()
 void Fire::displayColumnHeadings(ostream& out)
 {
 	// your code here
+	std::cout << "name                     district        longitude       latitude\n"
+		  << "----------------------   -------------   -------------   -------------\n";
 }
 
 std::ostream& operator<<(std::ostream& out, District district)
---
+++ asgmt01/fireTest.cpp
@@ -0,0 +1,11 @@
+/*
+	DELETE THIS FILE
+*/
+
+#include<iostream>
+#include"fire.h"
+
+int main(){
+	Fire fire("district", "name", 100, 10, 1, 200, 20, 2);
+	std::fire;
+}

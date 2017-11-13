0d4dab8a41ed12e8baf69635913ea84f0cfe447a
JacobTiderman <jacoberiktiderman@gmail.com>
2017-11-12 21:14:46 -0800

    date compiled

--- asgmt03/date.cpp
+++ asgmt03/date.cpp
@@ -3,16 +3,16 @@
 using namespace std;
 
 Date::Date(Month month, int day, int year) :
-	this->month{month},
-	this->day{day},
-	this->year{year}
+	/*this->*/month{month},
+	/*this->*/day{day},
+	/*this->*/year{year}
 {
 }
 
 Date::Date() :
-	this->month{Month::INVALID},
-	this->day{0},
-	this->year{0}
+	/*this->*/month{Month::INVALID},
+	/*this->*/day{0},
+	/*this->*/year{0}
 {
 }
 
@@ -23,6 +23,6 @@ Date::~Date(void)
 
 ostream& operator<<(ostream& out, const Date& d)
 {
-	out << d.month << " " << d.day << ", " << year;
+	out << d.month << " " << d.day << ", " << d.year;
 	return out;
 }

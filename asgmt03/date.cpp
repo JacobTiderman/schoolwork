76574375c742885876f550c683ae670e5e237083
JacobTiderman <jacoberiktiderman@gmail.com>
2017-11-12 21:05:50 -0800

    Completed but not tested date.cpp

--- asgmt03/date.cpp
+++ asgmt03/date.cpp
@@ -2,19 +2,27 @@
 
 using namespace std;
 
-Date::Date(Month month, int day, int year)
+Date::Date(Month month, int day, int year) :
+	this->month{month},
+	this->day{day},
+	this->year{year}
 {
 }
 
-Date::Date()
+Date::Date() :
+	this->month{Month::INVALID},
+	this->day{0},
+	this->year{0}
 {
 }
 
 Date::~Date(void)
 {
+	//Not needed
 }
 
 ostream& operator<<(ostream& out, const Date& d)
 {
+	out << d.month << " " << d.day << ", " << year;
 	return out;
 }
